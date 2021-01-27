f0c692da24e63dde48f2ff48690eb28f1cd787d8
l-k-mist <dylan@vandenbosch.co.za>
2021-01-27 09:40:42 +0200

    Tidy, tweak, comment.

--- arduino-bluetooth/arduino-bluetooth.ino
+++ arduino-bluetooth/arduino-bluetooth.ino
@@ -1,31 +1,34 @@
 #include <SoftwareSerial.h>
 #include "BluetoothSerial.h"
 
-SoftwareSerial BluetoothDevice(3,4);
+const int8_t bluetoothTxPin = 3;
+const int8_t bluetoothRxPin = 4;
+
+SoftwareSerial BluetoothDevice(bluetoothTxPin, bluetoothRxPin);
 
 // onMessageReceived is a callback function.
 // This is where you decide the message logic.
 // Each message has a maximum of three comma-separated parts.
 // Note that I haven't used part 3 at all, since - for now - I only need parts one and two.
 void onMessageReceived(char * messageParts[3], SoftwareSerial* device){
-    if(strcmp(messageParts[0], "Hi from web") == 0){ // In this strange world strcmp is 0 if the comparison is true.
-        Serial.println("Yay web says hi!");
-        device->println("Hi from arduino");
-    }
-    if(strcmp(messageParts[0], "echo") == 0){
-        device->print("I echo back    ");
-        device->println(messageParts[1]);
-    }
-    if(strcmp(messageParts[0], "setTemp") == 0){ 
-        device->print("Aiming for ");
-        device->print(atof(messageParts[1]));
-        device->println("C");
-    }
-    if(strcmp(messageParts[0], "getTemp") == 0){
-        Serial.println("I must fetch tempC...");
-        device->print("TempC: ");
-        // Heater.getTemp();
-    }
+  if(strcmp(messageParts[0], "Hi from web") == 0){ // In this strange world strcmp is 0 if the comparison is true.
+      Serial.println("Yay web says hi!");
+      device->println("Hi from arduino");
+  }
+  if(strcmp(messageParts[0], "echo") == 0){
+      device->print("I echo back    ");
+      device->println(messageParts[1]);
+  }
+  if(strcmp(messageParts[0], "setTemp") == 0){ 
+      device->print("Aiming for ");
+      device->print(atof(messageParts[1]));
+      device->println("C");
+  }
+  if(strcmp(messageParts[0], "getTemp") == 0){
+      Serial.println("I must fetch tempC...");
+      device->print("TempC: ");
+      // Heater.getTemp(); // You might want to take actions based on messages using other libraries.
+  }
 }
 // &BluetoothDevice is dependency injection and composition. myBluetooth has a BluetoothDevice; and has a callback.
 Bluetooth myBluetooth(&BluetoothDevice, onMessageReceived);
